b36ab743f4a880c187129dee85592e24b53eae2c
Joel Stockamp and sofia giordano <joel@>
2019-08-20 15:07:08 -0700

    styles with css

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,30 @@
 $(document).ready(function() {
+
+  $('.reset').click(function() {
+      window.location.reload();
+  });
+
   $('#burgerOrder').submit(function(event) {
     event.preventDefault();
 
     $('#burgerOrder').hide();
     $('.result').show();
 
-
     $('input:checkbox[name=proteinType]:checked').each(function() {
       var proteinType = $(this).val();
-      $('#finalOrder').append('<li>'+proteinType+'</li>');
-
-
+      $('#finalOrder').append('<li>' + proteinType + '</li>');
     });
-
 
     $('input:checkbox[name=bunType]:checked').each(function() {
       var bunType = $(this).val();
-      $('#finalOrder').append('<li>'+bunType+'</li>');
+      $('#finalOrder').append('<li>' + bunType + '</li>');
       console.log(bunType);
     });
 
     $('input:checkbox[name=toppings]:checked').each(function() {
       var toppings = $(this).val();
-      $('#finalOrder').append('<li>'+toppings+'</li>');
+      $('#finalOrder').append('<li>' + toppings + '</li>');
 
     });
-
-
-
-
   });
 });
